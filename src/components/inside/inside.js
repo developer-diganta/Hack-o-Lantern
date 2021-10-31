e319e991492899b9455c4d286b39ab1198f17b32
surya-doc <suryadanabnk@gmail.com>
2021-10-31 16:58:08 +0530

    Adding planchet part

--- src/components/inside/inside.js
+++ src/components/inside/inside.js
@@ -1,38 +1,51 @@
 import React from 'react'
 import "./inside.css"
 import { useHistory } from 'react-router';
+import Sound from 'react-sound';
 export default function Inside() {
     const history = useHistory();
+    function playMusic(){
+        var doorOpening = new Audio('scary_wooden.wav');
+        doorOpening.play();
+    }
     return (
         <div className="body">
             <div className="background">
+            <Sound
+                    url="scary_wind.wav"
+                    playStatus={Sound.status.PLAYING}
+                    // playFromPosition={200}
+                    loop={true}
+                />
                 <div className="text2">
-                    You See 4 Doors..... Enter into one....A choice you must make...
+                    You See 3 Doors..... Enter into one....A choice you must make...
                     <div className="choice">
                         <div className="door" onClick={()=>{
                             history.push("/roomone")
-
+                            playMusic()
                         }}>
                             Door 1
                         </div>
                         <div className="door" onClick={()=>{
                             history.push("/movie")
-
+                            playMusic()
                         }}>
                             Door 2
                         </div>
                         <div className="door" onClick={()=>{
-                            history.push("/")
+                            history.push("/planchet")
+                            playMusic()
 
                         }}>
                             Door 3
                         </div>
-                        <div className="door" onClick={()=>{
+                        {/* <div className="door" onClick={()=>{
                             history.push("/")
+                            playMusic()
 
                         }}>
                             Door 4
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
